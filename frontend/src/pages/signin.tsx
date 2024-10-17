7986ffb22555a41fdd9d4cf0dd14f55832004f1f
walianitin <walianitin828@gmail.com>
2024-10-17 11:39:49 +0530

    added fe

--- frontend/src/pages/signin.tsx
+++ frontend/src/pages/signin.tsx
@@ -1,21 +1,41 @@
+import Bottom from "../components/Bottom";
 import Button from "../components/Button";
 import Heading from "../components/Heading";
 import Input from "../components/Input";
-import Bottom from "../components/Bottom";
-function buttonClick()
-{
-   // console.log(buttonclicked)
-    <alert className="log">alert</alert>
+import Quotes from "../components/Quotes"; // Quotes component needs to have a heading and subheading prop
+
+function buttonClick   () {
+    alert("Button clicked");
 }
 
 export default function Signin() {
+    connst 
     return (
-        <div>
-                <Heading label={"Signin"}></Heading>
-               <Input label={"enter your email"} placeholder={"123@gmail.com"}></Input>
-               <Input label={"enter your password"} placeholder={"122Ad@@"}></Input>
-               <Button onClick={buttonClick} label={"Signin"}></Button>
-               <Bottom label={"Create a account "} to={"/signup"} linktext={"Signup"}> </Bottom>
+    <div className="justify-between mx-2 ">
+        <div className="flex flex-row items-center">
+            {/* Left Side: Form */}
+            <div className="flex flex-col p-12 rounded-lg w-1/2">
+                <div className="mb-6  flex flex-col items-center justify-center ">
+                    <Heading label="Login " ></Heading>
+                        <div className="flex flex-row p-1"> <Bottom label="Create an account?" linktext="Signup" to="/signup"></Bottom></div>
+                 </div>
+               
+                {/* <Input label="Username" placeholder="Enter your username"  ></Input> */}
+                <Input label="Email" placeholder="m@example.com" ></Input>
+                <Input label="Password" placeholder="Enter your password"></Input>
+               
+               <div className="p-3"><Button label="Sign In" onClick={buttonClick}></Button></div>
+            </div>
+
+            {/* Right Side: Quote */}
+            <div className=" flex items-center justify-center w-1/2 bg-slate-200 h-screen shadow-md p-4 text-center text-2xl italic">
+                <Quotes 
+                    heading="“The customer service I received was exceptional. The support team went above and beyond to address my concerns.”" 
+                    subHeading="Jules Winnfield, CEO, Acme Inc"
+                    className="text-center text-lg"
+                    />
+            </div>
         </div>
-    )
+    </div>
+    );
 }
--- frontend/src/pages/signup.tsx
+++ frontend/src/pages/signup.tsx
@@ -1,26 +1,60 @@
+import { useState } from "react";
 import Bottom from "../components/Bottom";
 import Button from "../components/Button";
 import Heading from "../components/Heading";
 import Input from "../components/Input";
-import SubHeading from "../components/SubHeading";
-import Signin from "./signin";
-function buttonClick()
-{
-   // console.log(buttonclicked)
-    <alert className="log">alert</alert>
+import Quotes from "../components/Quotes"; // Quotes component needs to have a heading and subheading prop
+import { signupInput} from "@nitinwalia00/common"
+import { string } from "zod";
+function buttonClick() {
+    alert("Button clicked");
+}
+
+interface labeleedinput{
+    label:string,
+    placeholder:string,
+    onChange: (e:ChangeEvent<HTMLInputElement>)=>void;
+    type?:string
 }
 
 export default function Signup() {
-    return (
-        <>
-        <Heading label="Signup"></Heading>
-        <SubHeading label= "enter your information to create a account"></SubHeading>
-        <Input label="enter you name " placeholder="Nitin"></Input>
-        <Input label="enter your email " placeholder="nitin@gmail.com"></Input>
-        <Input label="enter your password " placeholder="123f@saf"></Input>
-        <Button label={"Signup"} onClick={buttonClick}></Button>
-        <Bottom label="Already have an account?" linktext="Signin" to={"/signin"}></Bottom>
+    const [postInputs,setpostInputs]=useState({ 
+        name:"",
+        password:"",
+        email:""  
+    }) 
+    return (  
 
-        </>
-    )
+    <div className="justify-between mx-2 ">
+        <div className="flex flex-row items-center">
+            {/* Left Side: Form */}
+            <div className="flex flex-col p-12 rounded-lg w-1/2">
+                <div className="mb-6  flex flex-col items-center justify-center ">
+                    <Heading label="Create an account" ></Heading>
+                        <div className="flex flex-row p-1"> <Bottom label="Already have an account?" linktext="Login" to="/signin"></Bottom></div>
+                 </div>
+               
+                <Input label="Username" placeholder="Enter your username" onChange={(e)=>{
+                        setpostInputs({
+                            ...postInputs,
+                            name:e.target.value
+                        })
+                }}></Input>
+                <Input label="Email" placeholder="m@example.com" ></Input>
+                <Input label="Password" placeholder="Enter your password" type="password">  </Input>
+               
+               <div className="p-3"><Button label="Sign Up" onClick={buttonClick}></Button></div>
+            </div>
+
+            {/* Right Side: Quote */}
+            <div className=" flex items-center justify-center w-1/2 bg-slate-200 h-screen shadow-md p-4 text-center text-2xl italic">
+                <Quotes 
+                    heading="“The customer service I received was exceptional. The support team went above and beyond to address my concerns.”" 
+                    subHeading="Jules Winnfield, CEO, Acme Inc"
+                    className="text-center text-lg"
+                    />
+            </div>
+        </div>
+    </div>
+    );
 }
