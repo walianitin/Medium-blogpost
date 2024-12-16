e2cb401179b5a4f93292b996ada2357b4c149ead
walianitin <walianitin828@gmail.com>
2024-12-16 14:55:13 +0530

    signup page

--- frontend/src/components/Input.tsx
+++
@@ -1,7 +0,0 @@
-export default function Input({label,placeholder,onChange,type})
-{
-    return <div className="  flex flex-col shadow-sm p-3 text-start m-2 shadow-slate-200">
-         <div>{label}</div>
-         <div><input placeholder={placeholder}  type={type} onChange={onChange}></input></div> 
-    </div>
-}
