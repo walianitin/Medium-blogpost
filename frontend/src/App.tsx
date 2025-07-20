eb645c61e23d5cdaa1c756e66be70d465d7fda57
walianitin <walianitin828@gmail.com>
2025-07-20 12:05:30 +0530

    added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter ,Route,Routes} from "react-router-dom"
 import SignIn from "./components/Signup";  // Note the capital S and I
 import SignUp from "./components/Signup";  // Note the capital S and I
 import DashBoard from "./components/pages/DashBoard"
-import Header from "./components/Header"
 import Footer from "./components/footer";
 import Home from "./components/HomePage";
 import Write from "./components/pages/Write";
@@ -10,9 +9,9 @@ function App() {
   return (
     <>
     <BrowserRouter>
-      <Header />
+   
       <Routes>
-        <Route path="/" element={<Home></Home>}/>
+        <Route path="/" element={<DashBoard />}/>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/home" element={<DashBoard />} />
